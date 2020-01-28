a53c3d46267a5b8d898bd02770b3118a13ecf767
Chester Moon <cmoon3740@gmail.com>
2020-01-27 21:14:12 -0800

    fleshed out initial questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [{
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which of the following function of Number object formats a number with a specific number of digits to the right of the decimal?",
+        choices: ["toExponential()", "toFixed()", "toPrecision()", "toLocaleString()"],
+        answer: "toFixed()"
+    },
+    {
+        title: " Which of the following function of Boolean object returns the primitive value of the Boolean object?",
+        choices: ["toSource()", "valueOf()", "toString()", "None of the Above"],
+        answer: "toString()"
+    },
+    {
+        title: "Which of the following function of String object returns the index within the calling String object of the first occurrence of the specified value?",
+        choices: ["substr()", "search()", "lastIndexOf()", "indexOf()"],
+        answer: "indexOf()"
+    }
+
+]
