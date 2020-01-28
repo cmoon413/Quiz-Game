0a449853e1aede1f927a46be71a1598791b67f5f
Chester Moon <cmoon3740@gmail.com>
2020-01-28 14:35:50 -0800

    added listener for buttons and created index to progress quiz.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,7 +5,7 @@ var answerOne = document.querySelector('#answer1')
 var answerTwo = document.querySelector('#answer2')
 var answerThree = document.querySelector('#answer3')
 var answerFour = document.querySelector('answer4')
-
+var questionIndex = 0
 function quizTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -19,6 +19,7 @@ function quizTime() {
     }, 1000);
 }
 function displayQuestions(position) {
+
     for (let i = 0; i < questions[position].choices.length; i++) {
         answerList.children[i].textContent = questions[position].choices[i]
         
@@ -26,4 +27,24 @@ function displayQuestions(position) {
 
 }
 
+answers.addEventListener("click", function(event) {
+    if(event.target.textContent === questions[questionIndex].answer) {
+        console.log('correct')
+        questionIndex++
+        if(questionIndex < questions.length){
+            displayQuestions(questionIndex)
+            } else{
+            console.log('end')
+        }
+    } else {
+        console.log('Incorrect')
+        questionIndex++;
 
+        if (questionIndex < questions.length){
+        displayQuestions(questionIndex)
+        }   else {
+            console.log('end')
+        }
+    }
+  });
+  displayQuestions(questionIndex)
