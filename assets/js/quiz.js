5de6e83437c97cf4dac82c00877d8bb41ca302e1
Chester Moon <cmoon3740@gmail.com>
2020-01-29 20:35:14 -0800

    added question content to display

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,9 @@
 var countdown = document.querySelector('#time')
 var secondsLeft = (questions.length + 1) * 15
 var answerList = document.querySelector('#answers')
-var answerOne = document.querySelector('#answer1')
-var answerTwo = document.querySelector('#answer2')
-var answerThree = document.querySelector('#answer3')
-var answerFour = document.querySelector('answer4')
+var question = document.querySelector('#question')
 var questionIndex = 0
+
 function quizTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -18,33 +16,34 @@ function quizTime() {
 
     }, 1000);
 }
-function displayQuestions(position) {
 
+function displayQuestions(position) {
+    question.textContent = questions[position].title
     for (let i = 0; i < questions[position].choices.length; i++) {
         answerList.children[i].textContent = questions[position].choices[i]
-        
+
     }
 
 }
 
 answers.addEventListener("click", function(event) {
-    if(event.target.textContent === questions[questionIndex].answer) {
+    if (event.target.textContent === questions[questionIndex].answer) {
         console.log('correct')
         questionIndex++
-        if(questionIndex < questions.length){
+        if (questionIndex < questions.length) {
             displayQuestions(questionIndex)
-            } else{
+        } else {
             console.log('end')
         }
     } else {
         console.log('Incorrect')
         questionIndex++;
 
-        if (questionIndex < questions.length){
-        displayQuestions(questionIndex)
-        }   else {
+        if (questionIndex < questions.length) {
+            displayQuestions(questionIndex)
+        } else {
             console.log('end')
         }
     }
-  });
-  displayQuestions(questionIndex)
+});
+displayQuestions(questionIndex)
